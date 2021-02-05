8dd1398a8b30c75ebcad80a6f2be6c2f14aa758b
Jessica M. Rudd, PhD <jrudd2@a42labs.io>
2021-02-05 12:56:30 -0500

    push local to remote

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='using GANs to create s synthetic data to protect sensitive data use-cases, i.e. finance and medical',
+    author='Jessica M. Rudd, PhD',
+    license='MIT',
+)
